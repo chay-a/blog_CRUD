14b04f373a340988c6d1fdc32aa4c19aa89c54db
Chay Auriane <chaya.07@protonmail.com>
2022-02-03 15:07:56 +0100

    Feat : blogPost controller working

---
+++ app/controllers/blogPostController.php
@@ -0,0 +1,4 @@
+<?php
+require 'app/persistences/blogPostData.php';
+
+require 'ressources/views/blogPost.tpl.php';
--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
 include 'config/database.php';
-$queryPage = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_ENCODED);
+$options = [
+    'action' =>FILTER_SANITIZE_ENCODED,
+    'id' =>FILTER_SANITIZE_ENCODED
+];
+$queryPage = filter_input_array(INPUT_GET,$options);
 $RoutePath = '';
 $routes = [
     NULL => ['app/controllers/homeController.php', 'Accueil', 'CV en ligne de Auriane Chay avec ses compétences'],
-    'hobby' => ['pages/hobby.php', 'Hobbies', 'Découvrez les hobbies d\'Auriane Chay'],
-    'contact' => ['pages/contact.php','Contact', 'Contactez Auriane Chay pour bénéficier de ses compétences.'],
+    'blogPost' => ['app/controllers/blogPostController.php', 'Hobbies', 'Découvrez les hobbies d\'Auriane Chay'],
+    'blogPostModify' => ['pages/contact.php','Contact', 'Contactez Auriane Chay pour bénéficier de ses compétences.'],
 ];
 
-if (isset($routes[$queryPage])){
-    $metaTitle = $routes[$queryPage][1];
-    $metaDescription = $routes[$queryPage][2];
-    $RoutePath = $routes[$queryPage][0];
+if (isset($routes[$queryPage['action']])){
+    $metaTitle = $routes[$queryPage['action']][1];
+    $metaDescription = $routes[$queryPage['action']][2];
+    $RoutePath = $routes[$queryPage['action']][0];
 } else {
     $metaTitle = 'Page non trouvée';
     $metaDescription = 'Désoléee, page non trouvée';
     $RoutePath = 'pages/404.php';
 }
 
-//include 'ressources/views/layouts/header.php';
 include $RoutePath;
-//include 'ressources/views/layouts/footer.php';
