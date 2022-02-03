8ba9e8dead7b526f07c374055e6e65a74976c121
Chay Auriane <chaya.07@protonmail.com>
2022-02-03 13:47:15 +0100

    Feat : show 10 last articles

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -1,5 +1,8 @@
 <?php
 function lastBlogPosts($dbh) {
-    $last10Articles = [];
+    $query = file_get_contents('database/lastBlogPosts.sql');
+    $SQLRequest = $dbh->prepare($query);
+    $SQLRequest->execute();
+    $last10Articles = $SQLRequest->fetchall(PDO::FETCH_ASSOC);
     return $last10Articles;
 }
