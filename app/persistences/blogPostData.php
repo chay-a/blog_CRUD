0ac0b5b0d24ef3a82542c56a28f214e95355dd8b
Chay Auriane <chaya.07@protonmail.com>
2022-02-03 14:50:33 +0100

    Refacto: changeable nb of articles shown

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -2,6 +2,7 @@
 function lastBlogPosts($dbh) {
     $query = file_get_contents('database/lastBlogPosts.sql');
     $SQLRequest = $dbh->prepare($query);
+    $SQLRequest->bindValue(1, 10, PDO::PARAM_INT);
     $SQLRequest->execute();
     $last10Articles = $SQLRequest->fetchall(PDO::FETCH_ASSOC);
     return $last10Articles;
