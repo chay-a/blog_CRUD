a9a8cbe8d7fb44a8438f3f1aac96d945ace6a0cd
Chay Auriane <chaya.07@protonmail.com>
2022-02-03 10:32:03 +0100

    Feat: homecontroller for articles

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'config/database.php';
 $queryPage = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_ENCODED);
 $RoutePath = '';
 $routes = [
-    NULL => ['pages/accueil.php', 'Accueil', 'CV en ligne de Auriane Chay avec ses compétences'],
+    NULL => ['app/controllers/homeController.php', 'Accueil', 'CV en ligne de Auriane Chay avec ses compétences'],
     'hobby' => ['pages/hobby.php', 'Hobbies', 'Découvrez les hobbies d\'Auriane Chay'],
     'contact' => ['pages/contact.php','Contact', 'Contactez Auriane Chay pour bénéficier de ses compétences.'],
 ];
@@ -18,6 +18,6 @@ if (isset($routes[$queryPage])){
     $RoutePath = 'pages/404.php';
 }
 
-include 'ressources/views/layouts/header.php';
+//include 'ressources/views/layouts/header.php';
 include $RoutePath;
-include 'ressources/views/layouts/footer.php';
+//include 'ressources/views/layouts/footer.php';
