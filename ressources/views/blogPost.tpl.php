63800a660ebfe76b955e1d27cdc98847065be276
Chay Auriane <chaya.07@protonmail.com>
2022-02-04 15:53:48 +0100

    Update : link to modify or delete

--- ressources/views/blogPost.tpl.php
+++ ressources/views/blogPost.tpl.php
@@ -1,5 +1,7 @@
 <?php
 include 'ressources/views/header.tpl.php'; ?>
+<a href="index.php?action=blogPostModify&id=<?= $articleQuery['ID']?>">Modifier l'article</a>
+<a href="index.php?action=blogPostDelete&id=<?= $articleQuery['ID']?>">Supprimer l'article</a>
 <h1><?= $articleQuery['Title'] ?></h1>
 <p><?= $articleQuery['Cont']?></p>
 <p><?= $articleQuery['Pseudo']?></p>
