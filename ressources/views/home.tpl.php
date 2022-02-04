a2818d4c91842aac0ee8af6c67c38e9356cf0066
Chay Auriane <chaya.07@protonmail.com>
2022-02-04 15:32:47 +0100

    feat : link to articles and creation

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,14 +1,16 @@
 <?php
-include 'ressources/views/header.tpl.php';
-        if (!empty($lastArticles)) :
-            foreach($lastArticles as $key => $value):?>
-<h1><?= $value["Title"]?></h1>
-            <p><?= $value["Cont"]?></p>
-            <p><?= $value["Pseudo"]?></p>
-    <?php endforeach;?>
-        <?php else :?>
-        <h1>Il n'y a pas de post</h1>
-        <?php endif;?>
+include 'ressources/views/header.tpl.php'; ?>
+<a href="index.php?action=blogPostCreate">Créer votre article</a><br>
 <?php
-            include 'ressources/views/footer.tpl.php';
+if (!empty($lastArticles)) :
+    foreach ($lastArticles as $value):?>
+        <a href="index.php?action=blogPost&id=<?= $value['ID'] ?>"><?= $value["Title"] ?></a>
+        <p><?= $value["Cont"] ?></p>
+        <p><?= $value["Pseudo"] ?></p>
+    <?php endforeach; ?>
+<?php else : ?>
+    <h1>Il n'y a pas de post</h1>
+<?php endif; ?>
+<?php
+include 'ressources/views/footer.tpl.php';
 ?>
