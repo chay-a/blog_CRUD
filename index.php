a6e3babfe2417315eaaf7b03f0498883a930f272
chay-a <chaya.07@protonmail.com>
2022-02-08 19:18:35 +0100

    Refacto : path

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
 include 'config/database.php';
-$queryPage = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_ENCODED);
+$queryAction = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_ENCODED);
 $RoutePath = '';
 $routes = [
-    NULL => ['app/controllers/homeController.php', 'Accueil', 'CV en ligne de Auriane Chay avec ses compétences'],
-    'blogPost' => ['app/controllers/blogPostController.php', 'Hobbies', 'Découvrez les hobbies d\'Auriane Chay'],
-    'blogPostModify' => ['app/controllers/blogPostModifyController.php','Contact', 'Contactez Auriane Chay pour bénéficier de ses compétences.'],
-    'blogPostCreate' => ['app/controllers/blogPostCreateController.php', '', ''],
-    'blogPostDelete' => ['app/controllers/blogPostDeleteController.php', '', ''],
-    'authorCreate' => ['app/controllers/authorCreateController.php', '','']
+    NULL => 'app/controllers/homeController.php',
+    'blogPost' => 'app/controllers/blogPostController.php',
+    'blogPostModify' => 'app/controllers/blogPostModifyController.php',
+    'blogPostCreate' => 'app/controllers/blogPostCreateController.php',
+    'blogPostDelete' => 'app/controllers/blogPostDeleteController.php',
+    'authorCreate' => 'app/controllers/authorCreateController.php'
 ];
-if (isset($queryPage['action'])){
-    if (isset($routes[$queryPage['action']])){
-        //$metaTitle = $routes[$queryPage['action']][1];
-        //$metaDescription = $routes[$queryPage['action']][2];
-        $RoutePath = $routes[$queryPage['action']][0];
-    } else {
-        //$metaTitle = 'Page non trouvée';
-        //$metaDescription = 'Désoléee, page non trouvée';
-        $RoutePath = 'pages/404.php';
-        echo 'wrong';
-    }
+if (isset($routes[$queryAction])) {
+    $RoutePath = $routes[$queryAction];
 } else {
-    $RoutePath = $routes[$queryPage][0];
+    $RoutePath = 'pages/404.php';
+    echo 'wrong';
 }
 
 include $RoutePath;
